896f79010f303dcbd94a6b4aac4b5ff3faa3ab82
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-03-18 10:34:14 -0700

    feat(eslint - prettier):  add support for prettier and eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import type { AppProps } from 'next/app'
-import '../ui/styles/main.css'
+import type { AppProps } from 'next/app';
+
+import '../ui/styles/main.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 const HomePage = () => {
   return (
-    <div className='h-screen bg-[#04060b] grid place-items-center'>
-      <h1 className='text-white text-3xl'>
+    <div className="h-screen bg-[#04060b] grid place-items-center">
+      <h1 className="text-white text-3xl">
         <strong>Template made by:</strong> Rogelio Samuel <span>(@sam_621)</span>
       </h1>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
