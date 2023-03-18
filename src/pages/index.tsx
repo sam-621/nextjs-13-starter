84055ee8e600e7dc7646044f9ba9ba2ab7e29030
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-03-17 22:49:48 -0700

    feat(tailwind):  add tailwind support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 const HomePage = () => {
   return (
-    <div>
-      <h1>
-        Rogelio Samuel <span>@sam_621</span>
+    <div className='h-screen bg-[#04060b] grid place-items-center'>
+      <h1 className='text-white text-3xl'>
+        <strong>Template made by:</strong> Rogelio Samuel <span>(@sam_621)</span>
       </h1>
     </div>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/ui/**/*.{js,ts,jsx,tsx}'],
+  plugins: [],
+}
